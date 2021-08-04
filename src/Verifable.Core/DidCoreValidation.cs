lumoin/did-common-dotnet

Reset first draft and sketches of DIDs, VCs and others
An initial implementation of DIDs

This is an initial implementation of DIDs and later
VCs and presentation exchange.

src/DotDecentralized.Core/DidCoreValidation.cs
@@ -1,0 +1,46 @@
+using DotDecentralized.Core.Did;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotDecentralized.Core
+{
+    /// <summary>
+    /// DID Core validation rules as specificied in https://www.w3.org/TR/did-core/.
+    /// </summary>
+    public static class DidCoreValidation
+    {
+        //TODO: Write a top level validation function, something like TryValidateConformingDidDocument()
+        //https://www.w3.org/TR/did-core/#conformance.
+
+        /// <summary>
+        /// Checks the first item is <see cref="DidCoreConstants.JsonLdContextFirstUri"/>.
+        /// </summary>
+        /// <typeparam name="T">An object derived from <see cref="Context"/>.</typeparam>
+        /// <param name="obj">The context to check.</param>
+        /// <param name="results">The validation results if there were any. An empty collection otherwise.</param>
+        /// <returns>True if validation succeeds. False otherwise.</returns>
+        /// <remarks>See more at <a href="https://www.w3.org/TR/did-core/#json-ld"/>.</remarks>
+        public static bool TryValidJsonLdUriAsFirst<T>([NotNull] T obj, out ICollection<ValidationResult> results) where T: Context
+        {
+            if(obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            var firstContext = obj.Contexes?[0] as string;
+            bool success = firstContext?.Equals(DidCoreConstants.JsonLdContextFirstUri, StringComparison.InvariantCultureIgnoreCase) == true;
+            results = new List<ValidationResult>();
+            if(!success)
+            {
+                results.Add(new ValidationResult($"Context does not specify \"{DidCoreConstants.JsonLdContextFirstUri}\" as its first URI. This is mandated by DID Core specification."));
+            }
+
+            return success;
+        }
+    }
+}

src/DotDecentralized.Core/JsonLdContextConverter.cs
@@ -1,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotDecentralized.Core.Did
+{
+    /// <summary>
+    /// Converts <see cref="Context" to and from JSON.
+    /// Based on DictionaryTKeyEnumTValueConverter
+    /// at https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-converters-how-to.
+    /// https://w3c.github.io/did-imp-guide/
+    /// </summary>
+    public class JsonLdContextConverter: JsonConverter<Context>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert == typeof(Context);
+        }
+
+
+        public override Context Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            //The DID JSON-LD context starts either with a single string, array of strings, array of objects and strings
+            //or is an object that can contain whatever elements.
+            var context = new Context { Contexes = new List<object>(), AdditionalData = new Dictionary<string, object>() };
+            var tokenType = reader.TokenType;
+            if(reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var propertyName = reader.GetString();
+                if(propertyName == "@context")
+                {
+                    _ = reader.Read();
+                }
+            }
+
+            if(tokenType == JsonTokenType.String)
+            {
+                var ctx = reader.GetString();
+                if(ctx != null)
+                {
+                    context.Contexes.Add(ctx);
+                }
+
+                return context;
+            }
+
+            if(tokenType == JsonTokenType.StartArray)
+            {
+                var strList = JsonSerializer.Deserialize<object[]>(ref reader);
+                if(strList != null)
+                {
+                    for(int i = 0; i < strList.Length; i++)
+                    {
+                        var s = strList[i];
+                        context.Contexes.Add(s);
+                    }
+                }
+
+                return context;
+            }
+
+            while(reader.Read())
+            {
+                if(reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return context;
+                }
+
+                if(reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException("JsonTokenType was not PropertyName");
+                }
+
+                var propertyName = reader.GetString();
+                if(string.IsNullOrWhiteSpace(propertyName))
+                {
+                    throw new JsonException("Failed to get property name");
+                }
+
+                _ = reader.Read();
+                object? val = ExtractValue(ref reader, propertyName, options);
+                if(val != null)
+                {
+                    context.AdditionalData.Add(propertyName, val);
+                }
+            }
+
+            return context;
+        }
+
+
+        public override void Write(Utf8JsonWriter writer, Context value, JsonSerializerOptions options)
+        {
+            //writer.WritePropertyName("@context");
+            if(value?.Contexes?.Count == 1)
+            {
+                writer.WriteStringValue((string)value.Contexes.ElementAt(0));
+            }
+            else if(value?.Contexes?.Count > 1)
+            {
+                writer.WriteStartArray();
+                for(int i = 0; i < value?.Contexes.Count; ++i)
+                {
+                    if (value.Contexes.ElementAt(i) is string)
+                    {
+                        writer.WriteStringValue((string)value.Contexes.ElementAt(i));
+                    }
+                    else
+                    {
+                        JsonSerializer.Serialize(writer, value.Contexes.ElementAt(i));
+                    }
+                }
+
+                writer.WriteEndArray();
+            }
+
+            if(value?.AdditionalData?.Count > 0)
+            {
+                JsonSerializer.Serialize(writer, value.AdditionalData);
+            }
+        }
+
+
+        [return: MaybeNull]
+        private static object? ExtractValue(ref Utf8JsonReader reader, string propertyName, JsonSerializerOptions options)
+        {
+            //https://github.com/dotnet/corefx/blob/master/src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterKeyValuePair.cs
+            switch(reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    if(reader.TryGetDateTime(out var date))
+                    {
+                        return date;
+                    }
+                    return reader.GetString();
+                case JsonTokenType.False:
+                    return false;
+                case JsonTokenType.True:
+                    return true;
+                case JsonTokenType.Null:
+                    return null;
+                case JsonTokenType.Number:
+                    if(reader.TryGetInt64(out var result))
+                    {
+                        return result;
+                    }
+                    return reader.GetDecimal();
+                case JsonTokenType.StartObject:
+                    return JsonSerializer.Deserialize(ref reader, typeof(object));
+
+                case JsonTokenType.StartArray:
+
+                    var list = new List<object>();
+                    while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+                    {
+                        list.Add(ExtractValue(ref reader, propertyName, options)!);
+                    }
+                    return list;
+                //return JsonSerializer.Deserialize(ref reader, typeof(object[]));
+                default:
+                    throw new JsonException($"'{reader.TokenType}' is not supported");
+            }
+        }
+    }
+}

src/DotDecentralized.Core/ThrowHelper.cs
@@ -1,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace DotDecentralized.Core
+{
+    /// <summary>
+    /// Helper methods to throw. Extracting throw makes conditional branches smaller, more inlinable and reduces
+    /// generic codegen size.
+    /// </summary>
+    /// <remarks>Based on https://github.com/dotnet/corefx/blob/e1991a4f9c5cff43908a03d2c787e8246cfa5583/src/System.Text.Json/src/System/Text/Json/ThrowHelper.cs
+    /// and https://github.com/dotnet/corefx/blob/master/src/System.Text.Json/src/System/Text/Json/ThrowHelper.Serialization.cs .</remarks>
+    public static class ThrowHelper
+    {
+        /// <summary>
+        /// Throws a JSON exception.
+        /// </summary>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowJsonException()
+        {
+            throw new JsonException();
+        }
+
+
+        /// <summary>
+        /// Throws a JSON exception with a message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowJsonException(string message)
+        {
+            throw new JsonException(message);
+        }
+    }
+}

src/DotDecentralized.Core/VerifiableCredentials.cs
@@ -1,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotDecentralized.Core
+{
+    //https://www.w3.org/TR/vc-data-model/#types
+    //All of these types MUST have type information.
+
+    public class Credential { }
+
+    public class VerifiableCredential: Credential { }
+
+    public class Presentation { }
+
+    public class VerifiablePresentation: Presentation { }
+
+    public class Proof { }
+
+    public class CredentialStatus { }
+
+    public class TermsOfUse { }
+
+    public class Evidence { }
+}

src/DotDecentralized.Core/VerificationRelationshipConverter.cs
@@ -1,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotDecentralized.Core.Did
+{
+    /// <summary>
+    /// Abc.
+    /// </summary>
+    /// <remarks>Refactor a hook here if customer converters for relationships are needed. Is this needed?</remarks>
+    public class VerificationRelationshipConverterFactory: JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeof(VerificationRelationship).IsAssignableFrom(typeToConvert);
+        }
+
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            return (JsonConverter)Activator.CreateInstance(
+                typeof(VerificationRelationshipConverter<>)
+                    .MakeGenericType(new Type[] { typeToConvert }),
+                    BindingFlags.Instance | BindingFlags.Public,
+                    binder: null,
+                    args: null,
+                    culture: null)!;
+        }
+    }
+
+
+    /// <summary>
+    /// Converts a <see cref="VerificationRelationship"/> to or from JSON.
+    /// </summary>
+    /// <typeparam name="TVerificationRelationship">The type of <see cref="VerificationRelationship"/> to convert.</typeparam>
+    public class VerificationRelationshipConverter<TVerificationRelationship>: JsonConverter<TVerificationRelationship> where TVerificationRelationship : VerificationRelationship
+    {
+        /// <inheritdoc/>
+        public override TVerificationRelationship Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if(reader.TokenType != JsonTokenType.String && reader.TokenType != JsonTokenType.StartObject)
+            {
+                ThrowHelper.ThrowJsonException();
+            }
+
+            //TODO: Need to add "JwsVerificationKey2020" and handle like verification relationship?
+            //This is an extension: https://identity.foundation/sidetree/spec/v0.1.0/
+            // https://w3c-ccg.github.io/ld-cryptosuite-registry/
+            object? constructorParameter = null;
+            if(reader.TokenType == JsonTokenType.String)
+            {
+                constructorParameter = reader.GetString() ?? string.Empty;
+            }
+            else if(reader.TokenType == JsonTokenType.StartObject)
+            {
+                constructorParameter = JsonSerializer.Deserialize<VerificationMethod>(ref reader, options);
+            }
+            else
+            {
+                //Make the reason be an unexpected token JSON token type.
+                ThrowHelper.ThrowJsonException();
+            }
+
+            return (TVerificationRelationship)Activator.CreateInstance(typeof(TVerificationRelationship), new object[] { constructorParameter! })!;
+        }
+
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, TVerificationRelationship value, JsonSerializerOptions options)
+        {
+            var converter = GetKeyConverter<VerificationMethod>(options);
+            var verification = (VerificationRelationship)value;
+
+            if(verification.IsEmbeddedVerification && verification.EmbeddedVerification != null)
+            {
+                //TODO: Fix this.
+#pragma warning disable CS8604 // Possible null reference argument.
+                converter.Write(writer, value.EmbeddedVerification, options);
+#pragma warning restore CS8604 // Possible null reference argument.
+            }
+            else if(!verification.IsEmbeddedVerification)
+            {
+                writer.WriteStringValue(verification.VerificationReferenceId);
+            }
+        }
+
+
+        private static JsonConverter<TVerificationMethod> GetKeyConverter<TVerificationMethod>(JsonSerializerOptions options) where TVerificationMethod : VerificationMethod
+        {
+            return (JsonConverter<TVerificationMethod>)options.GetConverter(typeof(TVerificationMethod));
+        }
+    }
+}

Lumoin/Verifiable

src/Verifable.Core/DidCoreValidation.cs
@@ -1,0 +1,43 @@
+using Verifable.Core.Did;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Verifable.Core
+{
+    /// <summary>
+    /// DID Core validation rules as specificied in https://www.w3.org/TR/did-core/.
+    /// </summary>
+    public static class DidCoreValidation
+    {
+        //TODO: Write a top level validation function, something like TryValidateConformingDidDocument()
+        //https://www.w3.org/TR/did-core/#conformance.
+
+        /// <summary>
+        /// Checks the first item is <see cref="DidCoreConstants.JsonLdContextFirstUri"/>.
+        /// </summary>
+        /// <typeparam name="T">An object derived from <see cref="Context"/>.</typeparam>
+        /// <param name="obj">The context to check.</param>
+        /// <param name="results">The validation results if there were any. An empty collection otherwise.</param>
+        /// <returns>True if validation succeeds. False otherwise.</returns>
+        /// <remarks>See more at <a href="https://www.w3.org/TR/did-core/#json-ld"/>.</remarks>
+        public static bool TryValidJsonLdUriAsFirst<T>([NotNull] T obj, out ICollection<ValidationResult> results) where T: Context
+        {
+            if(obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            var firstContext = obj.Contexes?[0] as string;
+            bool success = firstContext?.Equals(DidCoreConstants.JsonLdContextFirstUri, StringComparison.InvariantCultureIgnoreCase) == true;
+            results = new List<ValidationResult>();
+            if(!success)
+            {
+                results.Add(new ValidationResult($"Context does not specify \"{DidCoreConstants.JsonLdContextFirstUri}\" as its first URI. This is mandated by DID Core specification."));
+            }
+
+            return success;
+        }
+    }
+}

src/Verifable.Core/JsonLdContextConverter.cs
@@ -1,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Verifable.Core.Did
+{
+    /// <summary>
+    /// Converts <see cref="Context" to and from JSON.
+    /// Based on DictionaryTKeyEnumTValueConverter
+    /// at https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-converters-how-to.
+    /// https://w3c.github.io/did-imp-guide/
+    /// </summary>
+    public class JsonLdContextConverter: JsonConverter<Context>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert == typeof(Context);
+        }
+
+
+        public override Context Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            //The DID JSON-LD context starts either with a single string, array of strings, array of objects and strings
+            //or is an object that can contain whatever elements.
+            var context = new Context { Contexes = new List<object>(), AdditionalData = new Dictionary<string, object>() };
+            var tokenType = reader.TokenType;
+            if(reader.TokenType == JsonTokenType.PropertyName)
+            {
+                if(reader.ValueTextEquals("@context"))
+                //if(propertyName == "@context")
+                {
+                    _ = reader.Read();
+                }
+            }
+
+            if(tokenType == JsonTokenType.String)
+            {
+                var ctx = reader.GetString();
+                if(ctx != null)
+                {
+                    context.Contexes.Add(ctx);
+                }
+
+                return context;
+            }
+
+            if(tokenType == JsonTokenType.StartArray)
+            {
+                var strList = JsonSerializer.Deserialize<object[]>(ref reader);
+                if(strList != null)
+                {
+                    for(int i = 0; i < strList.Length; i++)
+                    {
+                        var s = strList[i];
+                        context.Contexes.Add(s);
+                    }
+                }
+
+                return context;
+            }
+
+            while(reader.Read())
+            {
+                if(reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return context;
+                }
+
+                if(reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException("JsonTokenType was not PropertyName");
+                }
+
+                var propertyName = reader.GetString();
+                if(string.IsNullOrWhiteSpace(propertyName))
+                {
+                    throw new JsonException("Failed to get property name");
+                }
+
+                _ = reader.Read();
+                object? val = ExtractValue(ref reader, propertyName, options);
+                if(val != null)
+                {
+                    context.AdditionalData.Add(propertyName, val);
+                }
+            }
+
+            return context;
+        }
+
+
+        public override void Write(Utf8JsonWriter writer, Context value, JsonSerializerOptions options)
+        {
+            //writer.WritePropertyName("@context");
+            if(value?.Contexes?.Count == 1)
+            {
+                writer.WriteStringValue((string)value.Contexes.ElementAt(0));
+            }
+            else if(value?.Contexes?.Count > 1)
+            {
+                writer.WriteStartArray();
+                for(int i = 0; i < value?.Contexes.Count; ++i)
+                {
+                    if (value.Contexes.ElementAt(i) is string)
+                    {
+                        writer.WriteStringValue((string)value.Contexes.ElementAt(i));
+                    }
+                    else
+                    {
+                        JsonSerializer.Serialize(writer, value.Contexes.ElementAt(i));
+                    }
+                }
+
+                writer.WriteEndArray();
+            }
+
+            if(value?.AdditionalData?.Count > 0)
+            {
+                JsonSerializer.Serialize(writer, value.AdditionalData);
+            }
+        }
+
+
+        [return: MaybeNull]
+        private static object? ExtractValue(ref Utf8JsonReader reader, string propertyName, JsonSerializerOptions options)
+        {
+            //https://github.com/dotnet/corefx/blob/master/src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterKeyValuePair.cs
+            switch(reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    if(reader.TryGetDateTime(out var date))
+                    {
+                        return date;
+                    }
+                    return reader.GetString();
+                case JsonTokenType.False:
+                    return false;
+                case JsonTokenType.True:
+                    return true;
+                case JsonTokenType.Null:
+                    return null;
+                case JsonTokenType.Number:
+                    if(reader.TryGetInt64(out var result))
+                    {
+                        return result;
+                    }
+                    return reader.GetDecimal();
+                case JsonTokenType.StartObject:
+                    return JsonSerializer.Deserialize(ref reader, typeof(object));
+
+                case JsonTokenType.StartArray:
+
+                    var list = new List<object>();
+                    while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+                    {
+                        list.Add(ExtractValue(ref reader, propertyName, options)!);
+                    }
+                    return list;
+                //return JsonSerializer.Deserialize(ref reader, typeof(object[]));
+                default:
+                    throw new JsonException($"'{reader.TokenType}' is not supported");
+            }
+        }
+    }
+}

src/Verifable.Core/ThrowHelper.cs
@@ -1,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace Verifable.Core
+{
+    /// <summary>
+    /// Helper methods to throw. Extracting throw makes conditional branches smaller, more inlinable and reduces
+    /// generic codegen size.
+    /// </summary>
+    /// <remarks>Based on https://github.com/dotnet/corefx/blob/e1991a4f9c5cff43908a03d2c787e8246cfa5583/src/System.Text.Json/src/System/Text/Json/ThrowHelper.cs
+    /// and https://github.com/dotnet/corefx/blob/master/src/System.Text.Json/src/System/Text/Json/ThrowHelper.Serialization.cs .</remarks>
+    public static class ThrowHelper
+    {
+        /// <summary>
+        /// Throws a JSON exception.
+        /// </summary>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowJsonException()
+        {
+            throw new JsonException();
+        }
+
+
+        /// <summary>
+        /// Throws a JSON exception with a message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowJsonException(string message)
+        {
+            throw new JsonException(message);
+        }
+    }
+}

src/Verifable.Core/VerifiableCredentials.cs
@@ -1,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifable.Core
+{
+    //https://www.w3.org/TR/vc-data-model/#types
+    //All of these types MUST have type information.
+
+    public class Credential { }
+
+    public class VerifiableCredential: Credential { }
+
+    public class Presentation { }
+
+    public class VerifiablePresentation: Presentation { }
+
+    public class Proof { }
+
+    public class CredentialStatus { }
+
+    public class TermsOfUse { }
+
+    public class Evidence { }
+}

src/Verifable.Core/VerificationRelationshipConverter.cs
@@ -1,0 +1,94 @@
+using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Verifable.Core.Did
+{
+    /// <summary>
+    /// A factory to create converters for verification relationships.
+    /// </summary>
+    /// <remarks>Refactor a hook here if customer converters for relationships are needed. Is this needed?</remarks>
+    public class VerificationRelationshipConverterFactory: JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeof(VerificationRelationship).IsAssignableFrom(typeToConvert);
+        }
+
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            return (JsonConverter)Activator.CreateInstance(
+                typeof(VerificationRelationshipConverter<>)
+                    .MakeGenericType(new Type[] { typeToConvert }),
+                    BindingFlags.Instance | BindingFlags.Public,
+                    binder: null,
+                    args: null,
+                    culture: null)!;
+        }
+    }
+
+
+    /// <summary>
+    /// Converts a <see cref="VerificationRelationship"/> to or from JSON.
+    /// </summary>
+    /// <typeparam name="TVerificationRelationship">The type of <see cref="VerificationRelationship"/> to convert.</typeparam>
+    public class VerificationRelationshipConverter<TVerificationRelationship>: JsonConverter<TVerificationRelationship> where TVerificationRelationship : VerificationRelationship
+    {
+        /// <inheritdoc/>
+        public override TVerificationRelationship Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if(reader.TokenType != JsonTokenType.String && reader.TokenType != JsonTokenType.StartObject)
+            {
+                ThrowHelper.ThrowJsonException();
+            }
+
+            //TODO: Need to add "JwsVerificationKey2020" and handle like verification relationship?
+            //This is an extension: https://identity.foundation/sidetree/spec/v0.1.0/
+            // https://w3c-ccg.github.io/ld-cryptosuite-registry/
+            object? constructorParameter = null;
+            if(reader.TokenType == JsonTokenType.String)
+            {
+                constructorParameter = reader.GetString() ?? string.Empty;
+            }
+            else if(reader.TokenType == JsonTokenType.StartObject)
+            {
+                constructorParameter = JsonSerializer.Deserialize<VerificationMethod>(ref reader, options);
+            }
+            else
+            {
+                //Make the reason be an unexpected token JSON token type.
+                ThrowHelper.ThrowJsonException();
+            }
+
+            return (TVerificationRelationship)Activator.CreateInstance(typeof(TVerificationRelationship), new object[] { constructorParameter! })!;
+        }
+
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, TVerificationRelationship value, JsonSerializerOptions options)
+        {
+            var converter = GetKeyConverter<VerificationMethod>(options);
+            var verification = (VerificationRelationship)value;
+
+            if(verification.EmbeddedVerification != null && verification.IsEmbeddedVerification)
+            {
+                //TODO: Fix this.
+#pragma warning disable CS8604 // Possible null reference argument.
+                converter.Write(writer, value.EmbeddedVerification, options);
+#pragma warning restore CS8604 // Possible null reference argument.
+            }
+            else if(!verification.IsEmbeddedVerification)
+            {
+                writer.WriteStringValue(verification.VerificationReferenceId);
+            }
+        }
+
+
+        private static JsonConverter<TVerificationMethod> GetKeyConverter<TVerificationMethod>(JsonSerializerOptions options) where TVerificationMethod : VerificationMethod
+        {
+            return (JsonConverter<TVerificationMethod>)options.GetConverter(typeof(TVerificationMethod));
+        }
+    }
+}
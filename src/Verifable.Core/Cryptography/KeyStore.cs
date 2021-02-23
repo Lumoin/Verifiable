079abb0effc538f60be72fdba475280d02b5110e
Veikko Eeva <veikko.eeva@lumoin.com>
2021-02-23 20:56:13 +0200

    Reset first draft and sketches of DIDs, VCs and others
    An initial implementation of DIDs
    
    This is an initial implementation of DIDs and later
    VCs and presentation exchange.

---
+++ src/Verifable.Core/Cryptography/KeyStore.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Verifable.Core.Cryptography
+{
+    public abstract class KeyStore { }
+}
---
+++ test/Verifable.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotSsi.Benchmarks
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
