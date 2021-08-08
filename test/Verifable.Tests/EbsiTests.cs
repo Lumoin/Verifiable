lumoin/did-common-dotnet

Reset first draft and sketches of DIDs, VCs and others
An initial implementation of DIDs

This is an initial implementation of DIDs and later
VCs and presentation exchange.

test/DotDecentralized.Tests/EbsiTests.cs
@@ -1,0 +1,73 @@
+using DotDecentralized.Core;
+using DotDecentralized.Core.Did;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace DotDecentralized.Tests
+{
+    /// <summary>
+    /// EBSI specific tests. The source is at <a href="https://api.ebsi.xyz/docs/?urls.primaryName=DID%20API#/DID/get-did-v1-identifier">EBSI DID API Swagger</a>.
+    /// </summary>
+    public class EbsiDidTests
+    {
+        /// <summary>
+        /// The reader should be able to deserialize all these test files correctly.
+        /// </summary>
+        /// <param name="didDocumentFileName">The DID document data file under test.</param>
+        /// <param name="didDocumentFileContents">The DID document data file contents.</param>
+        [Theory]
+        [FilesData(TestInfrastructureConstants.RelativeTestPathToDeprecated + "EBSI", "ebsi-did-1.json")]
+        public void CanRoundtripLegacyEbsiDid(string didDocumentFilename, string didDocumentFileContents)
+        {
+            TestInfrastructureConstants.ThrowIfPreconditionFails(didDocumentFilename, didDocumentFileContents);
+
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                PropertyNamingPolicy = new DefaultNamingNamingPolicy(Array.AsReadOnly(new JsonNamingPolicy[] { JsonNamingPolicy.CamelCase })),
+                Converters =
+                {
+                    new VerificationRelationshipConverterFactory(),
+                    new VerificationMethodConverter(),
+                    new ServiceConverterFactory(),
+                    new JsonLdContextConverter()
+                }
+            };
+
+            TestExtendedDidDocument? deseserializedDidDocument = JsonSerializer.Deserialize<TestExtendedDidDocument>(didDocumentFileContents, options);
+            string reserializedDidDocument = JsonSerializer.Serialize(deseserializedDidDocument, options);
+
+            //All the DID documents need to have an ID and a context.
+            Assert.NotNull(deseserializedDidDocument?.Id);
+            Assert.NotNull(deseserializedDidDocument?.Context);
+            Assert.NotNull(reserializedDidDocument);
+
+            //Currently EBSI DIDs have public key embedded in the main document.
+            //This is not valid, but this is handled as "extra" that goes into
+            //extension data element and can be handled from there. This checks
+            //that this really is possible.
+            //See further https://api.ebsi.xyz/docs/?urls.primaryName=DID%20API#/DID/get-did-v1-identifier
+            //for the document description and how this is deprecated at https://www.w3.org/TR/did-spec-registries/#publickey
+            //in favour of VerificationMethod.
+            //
+            //Also the contained key https://www.w3.org/TR/did-spec-registries/#ethereumaddress is deprecated in favour
+            //of https://www.w3.org/TR/did-spec-registries/#blockchainaccountid.
+            //
+            //OBS! And test for testing extra registry types that are not part of the core specification
+            //but that can nevertheless be part of core Nuget library.
+            Assert.Single(deseserializedDidDocument?.AdditionalData);
+            Assert.IsType<JsonElement>(deseserializedDidDocument!.AdditionalData!["publicKey"]);
+
+            var comparer = new JsonElementComparer();
+            using var originalDIDDocument = JsonDocument.Parse(didDocumentFileContents);
+            using var parsedReserializedDIDDocument = JsonDocument.Parse(reserializedDidDocument);
+            Assert.True(comparer.Equals(originalDIDDocument.RootElement, parsedReserializedDIDDocument.RootElement), $"File \"{didDocumentFilename}\" did not pass roundtrip test.");
+        }
+    }
+}

test/DotDecentralized.Tests/JsonElementComparer.cs
@@ -1,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace DotDecentralized.Tests
+{
+    /// <summary>
+    /// Does deep, semantic comparison between two JSON documents.
+    /// </summary>
+    /// <remarks>This code is adapted from https://stackoverflow.com/questions/60580743/what-is-equivalent-in-jtoken-deepequal-in-system-text-json. </remarks>
+    public class JsonElementComparer: IEqualityComparer<JsonElement>
+    {
+        public JsonElementComparer() : this(-1) { }
+
+        public JsonElementComparer(int maxHashDepth) => this.MaxHashDepth = maxHashDepth;
+
+        int MaxHashDepth { get; } = -1;
+
+        public bool Equals(JsonElement x, JsonElement y)
+        {
+            if(x.ValueKind != y.ValueKind)
+            {
+                return false;
+            }
+
+            switch(x.ValueKind)
+            {
+                case JsonValueKind.Null:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Undefined:
+                    return true;
+
+                // Compare the raw values of numbers, and the text of strings.
+                // Note this means that 0.0 will differ from 0.00 -- which may be correct as deserializing either to `decimal` will result in subtly different results.
+                // Newtonsoft's JValue.Compare(JTokenType valueType, object? objA, object? objB) has logic for detecting "equivalent" values,
+                // you may want to examine it to see if anything there is required here.
+                // https://github.com/JamesNK/Newtonsoft.Json/blob/master/Src/Newtonsoft.Json/Linq/JValue.cs#L246
+                case JsonValueKind.Number:
+                    return x.GetRawText() == y.GetRawText();
+
+                case JsonValueKind.String:
+                    return x.GetString() == y.GetString(); // Do not use GetRawText() here, it does not automatically resolve JSON escape sequences to their corresponding characters.
+
+                case JsonValueKind.Array:
+                    return x.EnumerateArray().SequenceEqual(y.EnumerateArray(), this);
+
+                case JsonValueKind.Object:
+                {
+                    // Surprisingly, JsonDocument fully supports duplicate property names.
+                    // I.e. it's perfectly happy to parse {"Value":"a", "Value" : "b"} and will store both
+                    // key/value pairs inside the document!
+                    // A close reading of https://tools.ietf.org/html/rfc8259#section-4 seems to indicate that
+                    // such objects are allowed but not recommended, and when they arise, interpretation of
+                    // identically-named properties is order-dependent.
+                    // So stably sorting by name then comparing values seems the way to go.
+                    var xPropertiesUnsorted = x.EnumerateObject().ToList();
+                    var yPropertiesUnsorted = y.EnumerateObject().ToList();
+                    if(xPropertiesUnsorted.Count != yPropertiesUnsorted.Count)
+                    {
+                        return false;
+                    }
+
+                    var xProperties = xPropertiesUnsorted.OrderBy(p => p.Name, StringComparer.Ordinal);
+                    var yProperties = yPropertiesUnsorted.OrderBy(p => p.Name, StringComparer.Ordinal);
+                    foreach(var (px, py) in xProperties.Zip(yProperties))
+                    {
+                        if(px.Name != py.Name)
+                        {
+                            return false;
+                        }
+
+                        if(!Equals(px.Value, py.Value))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+                default:
+                    throw new JsonException(string.Format("Unknown JsonValueKind {0}", x.ValueKind));
+            }
+        }
+
+
+        public int GetHashCode(JsonElement obj)
+        {
+            var hash = new HashCode(); // New in .Net core: https://docs.microsoft.com/en-us/dotnet/api/system.hashcode
+            ComputeHashCode(obj, ref hash, 0);
+            return hash.ToHashCode();
+        }
+
+
+        private void ComputeHashCode(JsonElement obj, ref HashCode hash, int depth)
+        {
+            hash.Add(obj.ValueKind);
+
+            switch(obj.ValueKind)
+            {
+                case JsonValueKind.Null:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Undefined:
+                    break;
+
+                case JsonValueKind.Number:
+                    hash.Add(obj.GetRawText());
+                    break;
+
+                case JsonValueKind.String:
+                    hash.Add(obj.GetString());
+                    break;
+
+                case JsonValueKind.Array:
+                    if(depth != MaxHashDepth)
+                    {
+                        foreach(var item in obj.EnumerateArray())
+                        {
+                            ComputeHashCode(item, ref hash, depth + 1);
+                        }
+                    }
+                    else
+                    {
+                        hash.Add(obj.GetArrayLength());
+                    }
+
+                    break;
+
+                case JsonValueKind.Object:
+                    foreach(var property in obj.EnumerateObject().OrderBy(p => p.Name, StringComparer.Ordinal))
+                    {
+                        hash.Add(property.Name);
+                        if(depth != MaxHashDepth)
+                        {
+                            ComputeHashCode(property.Value, ref hash, depth + 1);
+                        }
+                    }
+                    break;
+
+                default:
+                    throw new JsonException(string.Format("Unknown JsonValueKind {0}", obj.ValueKind));
+            }
+        }
+    }
+}

test/DotDecentralized.Tests/SidetreeTests.cs
@@ -1,0 +1,58 @@
+using DotDecentralized.Core;
+using DotDecentralized.Core.Did;
+using DotDecentralized.Sidetree;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace DotDecentralized.Tests
+{
+    /// <summary>
+    /// Sidetree specific tests. See https://identity.foundation/ion/explorer/ for documents.
+    /// </summary>
+    public class SidetreeTests
+    {
+        /// <summary>
+        /// The reader should be able to deserialize all these test files correctly.
+        /// </summary>
+        /// <param name="didDocumentFileName">The DID document data file under test.</param>
+        /// <param name="didDocumentFileContents">The DID document data file contents.</param>
+        [Theory(Skip = "Needs a change in DidCore.Servic implementation. See comments there.")]
+        [FilesData(TestInfrastructureConstants.RelativeTestPathToSidetree, "ion-1.json")]
+        public void CanRoundtripIonDid(string didDocumentFilename, string didDocumentFileContents)
+        {
+            TestInfrastructureConstants.ThrowIfPreconditionFails(didDocumentFilename, didDocumentFileContents);
+
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                PropertyNamingPolicy = new DefaultNamingNamingPolicy(Array.AsReadOnly(new JsonNamingPolicy[] { JsonNamingPolicy.CamelCase })),
+                Converters =
+                {
+                    new VerificationRelationshipConverterFactory(),
+                    new VerificationMethodConverter(),
+                    new ServiceConverterFactory(),
+                    new JsonLdContextConverter()
+                }
+            };
+
+            SideTreeDocument? deseserializedDidDocument = JsonSerializer.Deserialize<SideTreeDocument>(didDocumentFileContents, options);
+            string reserializedDidDocument = JsonSerializer.Serialize(deseserializedDidDocument, options);
+
+            //All the DID documents need to have an ID and a context.
+            Assert.NotNull(deseserializedDidDocument?.Context);
+            Assert.NotNull(deseserializedDidDocument?.DidDocument);
+            Assert.NotNull(reserializedDidDocument);
+
+            var comparer = new JsonElementComparer();
+            using var originalDIDDocument = JsonDocument.Parse(didDocumentFileContents);
+            using var parsedReserializedDIDDocument = JsonDocument.Parse(reserializedDidDocument);
+            Assert.True(comparer.Equals(originalDIDDocument.RootElement, parsedReserializedDIDDocument.RootElement), $"File \"{didDocumentFilename}\" did not pass roundtrip test.");
+        }
+    }
+}

Lumoin/Verifiable

test/Verifable.Tests/EbsiTests.cs
@@ -1,0 +1,69 @@
+using Verifable.Core;
+using Verifable.Core.Did;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Verifable.Tests
+{
+    /// <summary>
+    /// EBSI specific tests. The source is at <a href="https://api.ebsi.xyz/docs/?urls.primaryName=DID%20API#/DID/get-did-v1-identifier">EBSI DID API Swagger</a>.
+    /// </summary>
+    public class EbsiDidTests
+    {
+        /// <summary>
+        /// The reader should be able to deserialize all these test files correctly.
+        /// </summary>
+        /// <param name="didDocumentFileName">The DID document data file under test.</param>
+        /// <param name="didDocumentFileContents">The DID document data file contents.</param>
+        [Theory]
+        [FilesData(TestInfrastructureConstants.RelativeTestPathToDeprecated + "EBSI", "ebsi-did-1.json")]
+        public void CanRoundtripLegacyEbsiDid(string didDocumentFilename, string didDocumentFileContents)
+        {
+            TestInfrastructureConstants.ThrowIfPreconditionFails(didDocumentFilename, didDocumentFileContents);
+
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                PropertyNamingPolicy = new DefaultNamingNamingPolicy(Array.AsReadOnly(new JsonNamingPolicy[] { JsonNamingPolicy.CamelCase })),
+                Converters =
+                {
+                    new VerificationRelationshipConverterFactory(),
+                    new VerificationMethodConverter(),
+                    new ServiceConverterFactory(),
+                    new JsonLdContextConverter()
+                }
+            };
+
+            TestExtendedDidDocument? deseserializedDidDocument = JsonSerializer.Deserialize<TestExtendedDidDocument>(didDocumentFileContents, options);
+            string reserializedDidDocument = JsonSerializer.Serialize(deseserializedDidDocument, options);
+
+            //All the DID documents need to have an ID and a context.
+            Assert.NotNull(deseserializedDidDocument?.Id);
+            Assert.NotNull(deseserializedDidDocument?.Context);
+            Assert.NotNull(reserializedDidDocument);
+
+            //Currently EBSI DIDs have public key embedded in the main document.
+            //This is not valid, but this is handled as "extra" that goes into
+            //extension data element and can be handled from there. This checks
+            //that this really is possible.
+            //See further https://api.ebsi.xyz/docs/?urls.primaryName=DID%20API#/DID/get-did-v1-identifier
+            //for the document description and how this is deprecated at https://www.w3.org/TR/did-spec-registries/#publickey
+            //in favour of VerificationMethod.
+            //
+            //Also the contained key https://www.w3.org/TR/did-spec-registries/#ethereumaddress is deprecated in favour
+            //of https://www.w3.org/TR/did-spec-registries/#blockchainaccountid.
+            //
+            //OBS! And test for testing extra registry types that are not part of the core specification
+            //but that can nevertheless be part of core Nuget library.
+            Assert.Single(deseserializedDidDocument?.AdditionalData);
+            Assert.IsType<JsonElement>(deseserializedDidDocument!.AdditionalData!["publicKey"]);
+
+            var comparer = new JsonElementComparer();
+            using var originalDIDDocument = JsonDocument.Parse(didDocumentFileContents);
+            using var parsedReserializedDIDDocument = JsonDocument.Parse(reserializedDidDocument);
+            Assert.True(comparer.Equals(originalDIDDocument.RootElement, parsedReserializedDIDDocument.RootElement), $"File \"{didDocumentFilename}\" did not pass roundtrip test.");
+        }
+    }
+}

test/Verifable.Tests/JsonElementComparer.cs
@@ -1,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace Verifable.Tests
+{
+    /// <summary>
+    /// Does deep, semantic comparison between two JSON documents.
+    /// </summary>
+    /// <remarks>This code is adapted from https://stackoverflow.com/questions/60580743/what-is-equivalent-in-jtoken-deepequal-in-system-text-json. </remarks>
+    public class JsonElementComparer: IEqualityComparer<JsonElement>
+    {
+        public JsonElementComparer() : this(-1) { }
+
+        public JsonElementComparer(int maxHashDepth) => this.MaxHashDepth = maxHashDepth;
+
+        int MaxHashDepth { get; } = -1;
+
+        public bool Equals(JsonElement x, JsonElement y)
+        {
+            if(x.ValueKind != y.ValueKind)
+            {
+                return false;
+            }
+
+            switch(x.ValueKind)
+            {
+                case JsonValueKind.Null:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Undefined:
+                    return true;
+
+                // Compare the raw values of numbers, and the text of strings.
+                // Note this means that 0.0 will differ from 0.00 -- which may be correct as deserializing either to `decimal` will result in subtly different results.
+                // Newtonsoft's JValue.Compare(JTokenType valueType, object? objA, object? objB) has logic for detecting "equivalent" values,
+                // you may want to examine it to see if anything there is required here.
+                // https://github.com/JamesNK/Newtonsoft.Json/blob/master/Src/Newtonsoft.Json/Linq/JValue.cs#L246
+                case JsonValueKind.Number:
+                    return x.GetRawText() == y.GetRawText();
+
+                case JsonValueKind.String:
+                    return x.GetString() == y.GetString(); // Do not use GetRawText() here, it does not automatically resolve JSON escape sequences to their corresponding characters.
+
+                case JsonValueKind.Array:
+                    return x.EnumerateArray().SequenceEqual(y.EnumerateArray(), this);
+
+                case JsonValueKind.Object:
+                {
+                    // Surprisingly, JsonDocument fully supports duplicate property names.
+                    // I.e. it's perfectly happy to parse {"Value":"a", "Value" : "b"} and will store both
+                    // key/value pairs inside the document!
+                    // A close reading of https://tools.ietf.org/html/rfc8259#section-4 seems to indicate that
+                    // such objects are allowed but not recommended, and when they arise, interpretation of
+                    // identically-named properties is order-dependent.
+                    // So stably sorting by name then comparing values seems the way to go.
+                    var xPropertiesUnsorted = x.EnumerateObject().ToList();
+                    var yPropertiesUnsorted = y.EnumerateObject().ToList();
+                    if(xPropertiesUnsorted.Count != yPropertiesUnsorted.Count)
+                    {
+                        return false;
+                    }
+
+                    var xProperties = xPropertiesUnsorted.OrderBy(p => p.Name, StringComparer.Ordinal);
+                    var yProperties = yPropertiesUnsorted.OrderBy(p => p.Name, StringComparer.Ordinal);
+                    foreach(var (px, py) in xProperties.Zip(yProperties))
+                    {
+                        if(px.Name != py.Name)
+                        {
+                            return false;
+                        }
+
+                        if(!Equals(px.Value, py.Value))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+                default:
+                    throw new JsonException(string.Format("Unknown JsonValueKind {0}", x.ValueKind));
+            }
+        }
+
+
+        public int GetHashCode(JsonElement obj)
+        {
+            var hash = new HashCode(); // New in .Net core: https://docs.microsoft.com/en-us/dotnet/api/system.hashcode
+            ComputeHashCode(obj, ref hash, 0);
+            return hash.ToHashCode();
+        }
+
+
+        private void ComputeHashCode(JsonElement obj, ref HashCode hash, int depth)
+        {
+            hash.Add(obj.ValueKind);
+
+            switch(obj.ValueKind)
+            {
+                case JsonValueKind.Null:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Undefined:
+                    break;
+
+                case JsonValueKind.Number:
+                    hash.Add(obj.GetRawText());
+                    break;
+
+                case JsonValueKind.String:
+                    hash.Add(obj.GetString());
+                    break;
+
+                case JsonValueKind.Array:
+                    if(depth != MaxHashDepth)
+                    {
+                        foreach(var item in obj.EnumerateArray())
+                        {
+                            ComputeHashCode(item, ref hash, depth + 1);
+                        }
+                    }
+                    else
+                    {
+                        hash.Add(obj.GetArrayLength());
+                    }
+
+                    break;
+
+                case JsonValueKind.Object:
+                    foreach(var property in obj.EnumerateObject().OrderBy(p => p.Name, StringComparer.Ordinal))
+                    {
+                        hash.Add(property.Name);
+                        if(depth != MaxHashDepth)
+                        {
+                            ComputeHashCode(property.Value, ref hash, depth + 1);
+                        }
+                    }
+                    break;
+
+                default:
+                    throw new JsonException(string.Format("Unknown JsonValueKind {0}", obj.ValueKind));
+            }
+        }
+    }
+}

test/Verifable.Tests/SidetreeTests.cs
@@ -1,0 +1,54 @@
+using Verifable.Core;
+using Verifable.Core.Did;
+using Verifable.Sidetree;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Verifable.Tests
+{
+    /// <summary>
+    /// Sidetree specific tests. See https://identity.foundation/ion/explorer/ for documents.
+    /// </summary>
+    public class SidetreeTests
+    {
+        /// <summary>
+        /// The reader should be able to deserialize all these test files correctly.
+        /// </summary>
+        /// <param name="didDocumentFileName">The DID document data file under test.</param>
+        /// <param name="didDocumentFileContents">The DID document data file contents.</param>
+        [Theory(Skip = "Needs a change in DidCore.Service implementation. See comments there.")]
+        [FilesData(TestInfrastructureConstants.RelativeTestPathToSidetree, "ion-1.json")]
+        public void CanRoundtripIonDid(string didDocumentFilename, string didDocumentFileContents)
+        {
+            TestInfrastructureConstants.ThrowIfPreconditionFails(didDocumentFilename, didDocumentFileContents);
+
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                PropertyNamingPolicy = new DefaultNamingNamingPolicy(Array.AsReadOnly(new JsonNamingPolicy[] { JsonNamingPolicy.CamelCase })),
+                Converters =
+                {
+                    new VerificationRelationshipConverterFactory(),
+                    new VerificationMethodConverter(),
+                    new ServiceConverterFactory(),
+                    new JsonLdContextConverter()
+                }
+            };
+
+            SideTreeDocument? deseserializedDidDocument = JsonSerializer.Deserialize<SideTreeDocument>(didDocumentFileContents, options);
+            string reserializedDidDocument = JsonSerializer.Serialize(deseserializedDidDocument, options);
+
+            //All the DID documents need to have an ID and a context.
+            Assert.NotNull(deseserializedDidDocument?.Context);
+            Assert.NotNull(deseserializedDidDocument?.DidDocument);
+            Assert.NotNull(reserializedDidDocument);
+
+            var comparer = new JsonElementComparer();
+            using var originalDIDDocument = JsonDocument.Parse(didDocumentFileContents);
+            using var parsedReserializedDIDDocument = JsonDocument.Parse(reserializedDidDocument);
+            Assert.True(comparer.Equals(originalDIDDocument.RootElement, parsedReserializedDIDDocument.RootElement), $"File \"{didDocumentFilename}\" did not pass roundtrip test.");
+        }
+    }
+}