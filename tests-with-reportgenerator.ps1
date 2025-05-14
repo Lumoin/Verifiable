5fe084dcf97169ad829064d109922c4c50d78c79
Veikko Eeva <veikko.eeva@lumoin.com>
2025-05-14 17:18:15 +0300

    Update .NET 10 to Preview 4, update MSTest runner

--- tests-with-reportgenerator.ps1
+++ tests-with-reportgenerator.ps1
@@ -6,5 +6,5 @@ if (Test-Path $CoverageTargetDir) {
 }
 
 dotnet build Verifiable.sln
-dotnet tool run dotnet-coverage collect --output $CoverageOutputFile --output-format cobertura test/Verifiable.Tests/bin/Debug/net9.0/Verifiable.Tests.exe --report-trx --report-trx-filename testresults.trx --results-directory $CoverageTargetDir
+dotnet tool run dotnet-coverage collect --output $CoverageOutputFile --output-format cobertura test/Verifiable.Tests/bin/Debug/net10.0/Verifiable.Tests.exe --report-trx --report-trx-filename testresults.trx --results-directory $CoverageTargetDir
 dotnet reportgenerator -filefilters:"-**\obj\**;-**\*.g.cs" -assemblyfilters:"+Verifiable*" -reports:$CoverageOutputFile -targetdir:$CoverageTargetDir -reporttypes:"HtmlInline"
